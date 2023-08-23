61b8d0779f70b293ce697e91e644b905ff030356
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-23 13:08:07 -0400

    gitignore .env

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-const port = process.env.PORT || 3000
+
+require('dotenv').config();
 const server = require('./app');
-const { sequelize } = require("./db.js");
+const port = process.env.PORT
 
 async function main() {
     try {
-      await sequelize.sync({force: true});  
-      console.log("succesfully connected");
       server.listen(port, ()=> console.log(`server listening on port ${port}`))
     } catch (error) {
       console.error("Unable to connect to database");
-      server.listen(port, ()=> console.log(`server listening on port ${port}`))
     }
 }
 main();
