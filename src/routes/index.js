d57996af010f7bd69b501cf7964e6de0af3096fd
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-23 15:31:49 -0400

    re-routing controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const celsiustofahrenheit = require('./celsiustofahrenheit')
-const recognizenumber = require('./recognizenumber')
-const recognizecatordog = require('./recognizecatordog')
+const celsiustofahrenheit = require('../controllers/celsiustofahrenheit');
+const recognizenumber = require('../controllers/recognizenumber');
+const recognizecatordog = require('../controllers/recognizecatordog');
 
-router.use('/celsiustofahrenheit', celsiustofahrenheit)
-router.use('/recognizenumber', recognizenumber)
-router.use('/recognizecatordog', recognizecatordog)
+router.use('/celsiustofahrenheit', celsiustofahrenheit);
+router.use('/recognizenumber', recognizenumber);
+router.use('/recognizecatordog', recognizecatordog);
 
-module.exports = router
+module.exports = router;
