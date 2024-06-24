1e5c6f3cb9f3bae42637fd1b30a2f236d5f56d36
GwerhDev <gwerh.dev@gmail.com>
2024-06-24 11:08:39 -0400

    fixing issues

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 
 require('dotenv').config();
 const server = require('./app');
-const port = process.env.PORT
+const { port } = require('./config');
 
 async function main() {
-    try {
-      server.listen(port, ()=> console.log(`server listening on port ${port}`))
-    } catch (error) {
-      console.error("Unable to connect to database");
-    }
+  try {
+    server.listen(port, () => console.log(`server listening on port ${port}`))
+  } catch (error) {
+    console.error("Unable to connect to database");
+  }
 }
 main();
